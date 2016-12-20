bdde26a1644efb88dd3e99cc346ca2a4c46c684b
zhangbowen <522608487@qq.com>
2016-12-20 10:26:44 +0800

    add console

--- require应用/index.php
+++ require应用/index.php
@@ -130,6 +130,7 @@ $signPackage = $jssdk->GetSignPackage();
 		  });
 
 		function f1(){
+			
 			wx.getLocation({
 		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 		    success: function (res) {
