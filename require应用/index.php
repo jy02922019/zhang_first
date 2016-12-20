fd6da563e05a2aae906a448f3788276e6d7258ef
zhangbowen <522608487@qq.com>
2016-12-20 10:34:21 +0800

    f1

--- require应用/index.php
+++ require应用/index.php
@@ -83,7 +83,7 @@ $signPackage = $jssdk->GetSignPackage();
 		</a></figure>
 	</div> -->
 	
-	<button onclick="f1">获取地理位置</button>
+	<button onclick="f1()">获取地理位置</button>
 	
 	<script type="text/javascript">
 		wx.config({
@@ -130,7 +130,7 @@ $signPackage = $jssdk->GetSignPackage();
 		  });
 
 		function f1(){
-			
+			console.log(1);
 			wx.getLocation({
 		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 		    success: function (res) {
